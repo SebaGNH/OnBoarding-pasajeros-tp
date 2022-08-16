c45a0168178fc6bd82fb2bf65f2067bbf4c263f3
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-08-15 22:04:22 -0300

    se solucionó el selected index

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,10 +4,9 @@ const Formulario = ({pasajeros, modificar_pasajero}) => {
     
     let [nombre_y_apellido, actualizar_nombre_y_apellido] = useState("");
     let [dni, actualizar_dni] = useState("");
+    let [cbo_clase, modificar_cbo_clase] = useState("FirstClass");
     let [butaca, actualizar_butaca] = useState("");
     let clases =[ { id: 1,  tipo: 'FirstClass' },{ id: 2,  tipo: 'Business' },{ id: 3,  tipo: 'Turista' },{ id: 4,  tipo: 'Economy' } ];
-    const cbo_lista_clases = document.getElementById('cbo_lista_clases');
-
 
 
     const handle_input_nombre_y_apellido = (e)=>{
@@ -19,36 +18,39 @@ const Formulario = ({pasajeros, modificar_pasajero}) => {
     const handle_input_butaca = (e)=>{
         actualizar_butaca(e.target.value);
     };
-
+    const handle_Input_select = (e) =>{
+        modificar_cbo_clase(e.target.value);
+    }
 
     const limpiar_campos = () => {
         actualizar_nombre_y_apellido("");
         actualizar_dni("");
-        cbo_lista_clases.selectedIndex = 0;
         actualizar_butaca("");
     }
 
     const validar_campos = () => {
-
+        if (nombre_y_apellido === "") {
+            console.log("Nombre y apellido limpio");
+        }
     }
 
 
     const handle_submit = (e) => {
         e.preventDefault(); 
         const canidad_pasajeros = pasajeros.length + 1;
-        
-        const cbo_Seleccionado = cbo_lista_clases.options[cbo_lista_clases.selectedIndex].value;
+
         modificar_pasajero([
             ...pasajeros,
             {
                 id: canidad_pasajeros,
                 nombre_y_apellido,
                 dni,
-                tipo_clase: cbo_Seleccionado,
+                tipo_clase: cbo_clase,
                 butaca
             } 
         ]);
         limpiar_campos();
+        validar_campos();
     }
     return ( <>
         <form action="" onSubmit={handle_submit} >                    
@@ -56,7 +58,7 @@ const Formulario = ({pasajeros, modificar_pasajero}) => {
 
             <input type="text" name="dni" id="dni" value={dni} onChange={ (e)=>{ handle_input_dni(e)}} placeholder=" Dni"/>
 
-            <select name='cbo_lista_clases' id='cbo_lista_clases'>
+            <select name='cbo_lista_clases' id='cbo_lista_clases' onChange={handle_Input_select}>
                 <option value="0" key="0"> Seleccione un tipo</option>
                 {clases.map((clase)=>   <option value={clase.tipo} key={clase.id}> {clase.tipo}</option>   )}
             </select>
